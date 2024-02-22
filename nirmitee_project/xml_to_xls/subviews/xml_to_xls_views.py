b60d8a613cb358487ed39dd591041bdfbafd760d
aniket_gurav <aniket.g@codezen.in>
2024-02-22 12:42:24 +0530

    making dynamic code for xml to xls

--- nirmitee_project/xml_to_xls/subviews/xml_to_xls_views.py
+++ nirmitee_project/xml_to_xls/subviews/xml_to_xls_views.py
@@ -5,6 +5,8 @@ from django.db import transaction
 
 import pandas as pd
 
+from datetime import datetime
+
 from nirmitee_project.settings import BASE_DIR
 #rest_framework import 
 from rest_framework.response import Response
@@ -14,6 +16,25 @@ from rest_framework import status
 import xml.etree.ElementTree as ET
 from openpyxl import Workbook
 
+def date_format_converter(date_string):
+    date_object = datetime.strptime(date_string, "%Y%m%d").date()
+
+    formatted_date = date_object.strftime("%d/%m/%Y")
+    return formatted_date
+
+def check_object_is_dict_or_list(item):
+    if isinstance(item, (str, int, float)):
+        return "T"
+    elif isinstance(item, type(None)):
+        return "N"
+    elif isinstance(item, dict):
+        return "D"
+    elif isinstance(item, list):
+        return "L"
+    else:
+        return "False"
+
+    
 
 class ConverXMLtoXLSAPIView(APIView):
 
@@ -26,40 +47,6 @@ class ConverXMLtoXLSAPIView(APIView):
         with open(folder_path, 'r') as file:
             xml_data = file.read()
             xml_dict = xmltodict.parse(xml_data)
-            # json_data = xmltodict.unparse(xml_dict, pretty=True)
-            # print(xml_dict)
-
-            # flat_data = flatten(xml_dict)
-
-            # # Convert to DataFrame
-            # df = pd.DataFrame([flat_data])
-
-            # # df = pd.json_normalize(xml_dict)
-
-            # # Save DataFrame to Excel
-            # output_folder = 'static'
-            # file_name = 'output.xlsx'
-            # output_file_path = os.path.join(output_folder, file_name)
-            # df.to_excel(output_file_path, index=False)
-
-        # tree = ET.parse('./static/Input.xml')
-        # root = tree.getroot()
-
-        # # Create a new workbook
-        # wb = Workbook()
-        # ws = wb.active
-        # for row_index, child in enumerate(root.iter()):
-        #     if child.text:  # Check if the element contains text
-        #         for col_index, item in enumerate(child):
-        #             print(row_index+1, col_index+1, item.text)
-        #             ws.cell(row=row_index+1, column=col_index+1, value=item.text)
-
-        # file_name = os.path.basename('./static/Input.xml').split('.')[0] + '.xlsx'
-        # output_file_path = os.path.join('./static/output/', file_name)
-        # wb.save(output_file_path)
-
-
-
 
         workbook = Workbook()
         sheet = workbook.active
@@ -67,20 +54,68 @@ class ConverXMLtoXLSAPIView(APIView):
         # sheet.append(list(xml_dict['ENVELOPE']["BODY"]["IMPORTDATA"]["REQUESTDESC"]["STATICVARIABLES"]["SVCURRENTCOMPANY"]["VOUCHER"].keys()))
         sheet.append(list(xml_dict['ENVELOPE']["BODY"]["IMPORTDATA"]["REQUESTDESC"]["STATICVARIABLES"].keys()))
         sheet.append([xml_dict['ENVELOPE']["BODY"]["IMPORTDATA"]["REQUESTDESC"]["STATICVARIABLES"]["SVCURRENTCOMPANY"]])
+        sheet.append(["TALLYMESSAGE"])
         # print("xml_dict_voucher", xml_dict['ENVELOPE']["BODY"]["IMPORTDATA"]["REQUESTDATA"]["TALLYMESSAGE"])
-        try:
-            for item in xml_dict['ENVELOPE']["BODY"]["IMPORTDATA"]["REQUESTDATA"]["TALLYMESSAGE"]:
-                # print(item["VOUCHER"]["DATE"], item["VOUCHER"]["REFERENCEDATE"], item["VOUCHER"]["VOUCHERTYPENAME"])
-                # sheet.append([item["VOUCHER"]["DATE"], item["VOUCHER"]["REFERENCEDATE"], item["VOUCHER"]["VOUCHERTYPENAME"]])
-                sheet.append(["@xmlns:UDF", item["@xmlns:UDF"]])
-                sheet.append(['VOUCHER'])
-                sheet.append(['', '@REMOTEID', item["VOUCHER"].get("@REMOTEID",'')])
-                sheet.append(['', '@VCHKEY', item["VOUCHER"].get("@VCHKEY",'')])
-                sheet.append(['', '@VCHTYPE', item["VOUCHER"].get("@VCHTYPE",'')])
-                sheet.append(['', '@ACTION', item["VOUCHER"].get("@ACTION",'')])
-                sheet.append(['', '@OBJVIEW', item["VOUCHER"].get("@OBJVIEW",'')])
-        except:
-            pass
+        # try:
+        for item_level_1 in xml_dict['ENVELOPE']["BODY"]["IMPORTDATA"]["REQUESTDATA"]["TALLYMESSAGE"]:
+            response_item_level_1 = check_object_is_dict_or_list(item_level_1)
+            if response_item_level_1 == "D":
+                if len(item_level_1) > 1:
+                    for item_level_2 in item_level_1:
+
+                        response_item_level_2 = check_object_is_dict_or_list(item_level_1[item_level_2])
+                        if response_item_level_2 == "D":
+                            if len(item_level_2) > 1:
+
+
+                                for item_level_3 in item_level_1[item_level_2]:
+                                    response_item_level_3 = check_object_is_dict_or_list(item_level_1[item_level_2][item_level_3])
+                                    if response_item_level_3 in ["D", "T"] and len(item_level_3) > 1:
+                                        for item_level_4 in item_level_3:
+                                            continue
+                                            # response_item_level_4 = check_object_is_dict_or_list(item_level_4[item_level_3])
+                                            # if response_item_level_4 == "D":
+                                            #     if len(item_level_4) > 1:
+
+
+                                            #         for item_level_5 in item_level_4:
+                                            #             response_item_level_5 = check_object_is_dict_or_list(item_level_5[item_level_4])
+                                            #             if response_item_level_5 == "D":
+                                            #                 if len(item_level_5) > 1:
+
+
+                                            #                     for item_level_6 in item_level_5:
+                                            #                         continue
+
+
+                                            #             elif response_item_level_5 == "T":
+                                            #                 sheet.append(['','','','',item_level_5, item_level_4[item_level_5]])
+                                            #             else:
+                                            #                 print(response_item_level_5)
+
+
+                                            # elif response_item_level_4 == "T":
+                                            #     sheet.append(['','','', item_level_4, item_level_3[item_level_4]])
+                                            # else:
+                                            #     print(response_item_level_4)
+
+
+                                    elif response_item_level_3 in ["T", "N"]:
+                                        sheet.append(['','',item_level_1[item_level_2], item_level_1[item_level_2][item_level_3]])
+                                    else:
+                                        print(response_item_level_3)
+
+
+                        elif response_item_level_2 in ["T", "N"]:
+                            sheet.append(['',item_level_2, item_level_1[item_level_2]])
+                        else:
+                            print(response_item_level_2)
+                else:
+                    continue
+            else:
+                print("bad")
+        # except:
+        #     pass
 
         file_name = os.path.basename('./static/Input.xml').split('.')[0] + '.xlsx'
         output_file_path = os.path.join('./static/output/', file_name)
